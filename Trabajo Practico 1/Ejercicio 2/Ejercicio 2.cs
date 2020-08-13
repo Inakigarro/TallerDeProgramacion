b0e0af76cc7493563e03da40d2e5b209c02c2f37
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 00:33:51 -0300

    Create Ejercicio 2.cs

---
+++ Trabajo Practico 1/Ejercicio 2/Ejercicio 2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+
+            string entradaSwitch = Console.ReadLine();
+            int numero = int.Parse(entradaSwitch);
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("UNO");
+                    break;
+                case 2:
+                    Console.WriteLine("DOS");
+                    break;
+                case 3:
+                    Console.WriteLine("TRES");
+                    break;
+                case 4:
+                    Console.WriteLine("CUATRO");
+                    break;
+                case 5:
+                    Console.WriteLine("CINCO");
+                    break;
+                case 6:
+                    Console.WriteLine("SEIS");
+                    break;
+                case 7:
+                    Console.WriteLine("SIETE");
+                    break;
+                case 8:
+                    Console.WriteLine("OCHO");
+                    break;
+                case 9:
+                    Console.WriteLine("NUEVE");
+                    break;
+                default:
+                    Console.WriteLine("OTRO");
+                    break;
+
+            }
+        }
+    }
+}
