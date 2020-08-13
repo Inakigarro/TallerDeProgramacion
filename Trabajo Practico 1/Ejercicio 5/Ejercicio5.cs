50acd22dcf156931d76e9ba115e43d6a427816e4
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 18:25:27 -0300

    Create Ejercicio5.cs

---
+++ Trabajo Practico 1/Ejercicio 5/Ejercicio5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabajo_Practico_1
+{
+    class Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
