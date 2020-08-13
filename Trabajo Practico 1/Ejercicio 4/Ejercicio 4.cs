34726fcb8d6e5aa6bee8407265ea6f36a701846a
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 15:28:12 -0300

    Ejercicio 4

---
+++ Trabajo Practico 1/Ejercicio 4/Ejercicio 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            int cantidadNumeros = 1;
+            int resultado = 0;
+            int promedio;
+            int num = 1;
+            while (num <= 50)
+            {
+                resultado += num;
+                cantidadNumeros++; 
+            }
+            promedio = cantidadNumeros / resultado;
+            Console.WriteLine("La cantidad de numeros sumados es: " + cantidadNumeros);
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+            Console.WriteLine("El promedio es: " + promedio);
+
+        }
+    }
+}
