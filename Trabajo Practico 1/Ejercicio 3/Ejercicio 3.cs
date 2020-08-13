2dadd118c11edb55bf64ba6ab31cd3d8303b5648
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 13:34:54 -0300

    Update Ejercicio 3.cs

--- Trabajo Practico 1/Ejercicio 3/Ejercicio 3.cs
+++ Trabajo Practico 1/Ejercicio 3/Ejercicio 3.cs
@@ -6,12 +6,19 @@ namespace EjTrabajo_Practico_1ercicio_3
     { 
         static void Main()
         {
+            int cantidadNumeros = 0;
             int resultado = 0;
+            int promedio;
             for(int num = 1; num < 50; num++ )
             {
                 resultado += num;
+                cantidadNumeros++;
+                
             }
+            promedio = resultado / cantidadNumeros;
+            Console.WriteLine("Se sumaron " + cantidadNumeros + " numeros");
             Console.WriteLine("El resultado es: " + resultado);
+            Console.WriteLine("El promedio es: " + promedio);
         }
     }
 }
