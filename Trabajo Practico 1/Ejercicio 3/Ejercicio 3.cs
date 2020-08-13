215ae5001f980d52bfe35bbe6d9b208ca3458127
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 12:53:24 -0300

    Ejercicio 3

---
+++ Trabajo Practico 1/Ejercicio 3/Ejercicio 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EjTrabajo_Practico_1ercicio_3
+{
+    class Ejercicio3
+    { 
+        static void Main()
+        {
+            int resultado = 0;
+            for(int num = 1; num < 50; num++ )
+            {
+                resultado += num;
+            }
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+    }
+}
