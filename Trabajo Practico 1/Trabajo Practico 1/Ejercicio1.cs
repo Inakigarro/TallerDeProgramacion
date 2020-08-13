fec26a441bd9e210ae054543109075f3e29d6c30
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-13 00:33:45 -0300

    Create Ejercicio1.cs

---
+++ Trabajo Practico 1/Trabajo Practico 1/Ejercicio1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Trabajo_Practico_1
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+
+            string entradaIF = Console.ReadLine();
+            int numero = int.Parse(entradaIF);
+          
+        }
+    }
+}
