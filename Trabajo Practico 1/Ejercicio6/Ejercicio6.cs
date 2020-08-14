08b4d9992fae2403c36fd04f696b7ccafb10c647
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-14 18:59:26 -0300

    Update Ejercicio6.cs

--- Trabajo Practico 1/Ejercicio6/Ejercicio6.cs
+++ Trabajo Practico 1/Ejercicio6/Ejercicio6.cs
@@ -2,11 +2,26 @@
 
 namespace Ejercicio6
 {
-    class Program
+    class Ejercicio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero = 1;
+            int resultadoImpar = 0;
+            int cantidadNumerosImpar = 0;
+            while(numero < 100)
+            {
+                if ((numero % 2) != 0)
+                {
+                    resultadoImpar += numero;
+                    cantidadNumerosImpar++;
+                }
+                numero++;
+            }
+
+            Console.WriteLine("La cantidad de numeros impares sumados es: " + cantidadNumerosImpar);
+            Console.WriteLine("El resultado de la suma de numero impares es: " + resultadoImpar);
+            Console.WriteLine("El promedio de la suma es: " + (resultadoImpar / cantidadNumerosImpar));
         }
     }
 }
