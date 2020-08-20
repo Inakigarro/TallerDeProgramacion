b4dc721bc86f1b2db7869d997b81150a63c26e9d
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-20 11:22:21 -0300

    Update Ejercicio11.cs

--- Trabajo Practico 1/Ejercicio 11/Ejercicio11.cs
+++ Trabajo Practico 1/Ejercicio 11/Ejercicio11.cs
@@ -2,11 +2,18 @@
 
 namespace Ejercicio_11
 {
-    class Program
+    class Ejercicio11
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main()
+        {//Ingreso por consola el radio del circulo
+            Console.Write("Ingrese el radio del circulo: ");
+            double radio = double.Parse(Console.ReadLine());
+            //Calculo el area y el perimetro
+            double areaCirculo = (Math.PI * Math.Pow(radio, 2));
+            double perimetroCircunferencia = 2 * Math.PI * radio;
+            //Muestro por pantalla los resultados.
+            Console.WriteLine("El Area del circulo es: " + areaCirculo);
+            Console.WriteLine("El perimetro de la circunferencia es: " + perimetroCircunferencia);
         }
     }
 }
