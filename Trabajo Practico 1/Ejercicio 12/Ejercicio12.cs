d8c9d5275e086470290628721dcea83825093248
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-20 11:32:40 -0300

    Create Ejercicio12.cs

---
+++ Trabajo Practico 1/Ejercicio 12/Ejercicio12.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_12
+{
+    class Ejercicio12
+    {
+        static int Factorial(int n)
+        {
+            int x;
+            if (n == 0)
+            {
+                x = 1;
+                return (x);
+            }
+            else
+            {
+                x = Factorial(n - 1) * n;
+                return (x);
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            //Ingreso el numero al que quiero calcularle el factorial.
+            Console.Write("Ingrese un numero entero: ");
+            int numero = int.Parse(Console.ReadLine());
+            int factorial = Factorial(numero);
+            Console.WriteLine("El factorial de " + numero + " es: " + factorial);
+            
+        }
+    }
+}
