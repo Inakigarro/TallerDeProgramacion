b2dbf414d3416e3d074536f0e0c1e92b7d273039
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-20 08:01:41 -0300

    Create Ejercicio8.cs

---
+++ Trabajo Practico 1/Ejercicio 8/Ejercicio8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_8
+{
+    class Ejercicio8
+    { 
+        static void Main()
+        {
+           
+            Console.WriteLine("Ingrese la cantidad de numeros que desea calcular");
+            int c = int.Parse(Console.ReadLine());
+            int resultado;
+            for(int a = 0; a <= c; a++)
+            {
+                resultado = Fibonacci(a);
+                Console.WriteLine(resultado);
+            }
+
+        }
+        static int Fibonacci(int n)
+        {
+            int x;
+            if (n == 0)
+            {
+                x = 0;
+                return (x);
+            }
+            else if (n == 1)
+            {
+                x = 1;
+                return (x);
+            }
+            else
+            {
+                x = Fibonacci(n - 1) + Fibonacci(n - 2);
+                return (x);
+            }
+        }
+    }
+}
