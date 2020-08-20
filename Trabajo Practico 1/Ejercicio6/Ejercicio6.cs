655c329f288e856003c38ecf577452bd2b945107
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-20 09:22:58 -0300

    Update Ejercicio6.cs

--- Trabajo Practico 1/Ejercicio6/Ejercicio6.cs
+++ Trabajo Practico 1/Ejercicio6/Ejercicio6.cs
@@ -5,20 +5,22 @@ namespace Ejercicio6
     class Ejercicio6
     {
         static void Main(string[] args)
-        {
+        {//Inicializo las variables necesarias para el ejercicio.
             int numero = 1;
             int resultadoImpar = 0;
             int cantidadNumerosImpar = 0;
-            while(numero < 100)
-            {
+            //Mientras el numero sea menor o igual a 100
+            while(numero <= 100)
+            {//Un numero es impar si al dividirlo por 2 el resto es distinto de cero.
                 if ((numero % 2) != 0)
-                {
+                { //Sumo cada numero impar encontrado a la variable resultado. Aumento la cantidad de numeros sumados.
                     resultadoImpar += numero;
                     cantidadNumerosImpar++;
                 }
+                //Incremento la variable numero
                 numero++;
             }
-
+            //Muestro los resultados.
             Console.WriteLine("La cantidad de numeros impares sumados es: " + cantidadNumerosImpar);
             Console.WriteLine("El resultado de la suma de numero impares es: " + resultadoImpar);
             Console.WriteLine("El promedio de la suma es: " + (resultadoImpar / cantidadNumerosImpar));
