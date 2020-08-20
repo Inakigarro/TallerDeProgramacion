fce30e4a98bfd2a0da87a49b8c53f3cffc4f256f
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-20 09:45:03 -0300

    Update Ejercicio7.cs

--- Trabajo Practico 1/Ejercicio 7/Ejercicio7.cs
+++ Trabajo Practico 1/Ejercicio 7/Ejercicio7.cs
@@ -10,14 +10,15 @@ namespace Ejercicio_7
             Console.WriteLine("Cuantos terminos desea sumar?");
             int cantidadSumas = int.Parse(Console.ReadLine());
 
-            
+            //Inicializo las variables necesarias para el ejercicio.
             double resultado = 0;
             double aproximacionPorcentual;
-          
+            //Realizo la cantidad de sumas ingresadas para aproximar el numero Pi.
             for(int n = 0;  n <= cantidadSumas; n++)
             {
                 resultado += Math.Pow(-1, n) / ((2 * n) + 1);
             }
+            //Calculo y muestro los resultados.
             Console.WriteLine("El resultado de la suma de Leibniz es: " + resultado);
             aproximacionPorcentual = (resultado * 100) / Math.PI;
             Console.WriteLine("La aproximacion porcentual es: " + aproximacionPorcentual);
