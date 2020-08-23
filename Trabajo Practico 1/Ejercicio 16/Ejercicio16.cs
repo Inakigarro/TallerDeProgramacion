f8f417c92045ae46f4dc21287804aa9e203e225a
Iñaki Garro <60785409+Zaky-7@users.noreply.github.com>
2020-08-22 22:37:28 -0300

    Trabajo Practico1

---
+++ Trabajo Practico 1/Ejercicio 16/Ejercicio16.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Ejercicio_16
+{
+    class Ejercicio16
+    {
+        static void Main(string[] args)
+        {
+            char caracterAContar;
+            int cantidad = 0;
+            int posicionCadena = 0;
+            //Ingreso las cadenas por consola.
+            Console.WriteLine("Ingrese la primer cadena.");
+            string cadena1 = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la segunda cadena.");
+            string cadena2 = Console.ReadLine();
+
+            //Si las cadenas tienen distinto tamaño, no son anagramas.
+            if(cadena1.Length != cadena2.Length)
+            {
+                Console.WriteLine("No son anagramas."); ;
+            }
+            //Si tienen el mismo tamaño, debo revisar si los caracteres de la primera cadena se encuentran en los caracteres de la segunda.
+            else
+            {
+                //Recorro la primera cadena.
+                while(posicionCadena < cadena1.Length)
+                {
+                    //Extraigo el caracter que voy a revisar.
+                    caracterAContar = cadena1[posicionCadena];
+                    //Cuento cuantas veces se encuentra ese caracter en la cadena
+                    foreach(char c in cadena1)
+                    {
+                        if(c == caracterAContar)
+                        {
+                            cantidad++;
+                        }
+                    }
+                    //Recorro la segunda cadena en busca de la misma cantidad de caracteres.
+                    foreach(char c in cadena2)
+                    {
+                        while(cantidad != 0)
+                        {
+                            if(c == caracterAContar)
+                            {
+                                cantidad--;
+                            }
+                        }
+                    }
+                    //Si despues de recorrer la segunda cadena la cantidad es distinto de cero, entonces no son anagramas.
+                    if(cantidad != 0)
+                    {
+                        Console.WriteLine("No son anagramas.");
+                    }
+                    //Si cantidad es cero, entonces adelanto una posicion en la cadena1 y repito el proceso.
+                    else
+                    {
+                        posicionCadena++;
+                    }
+
+                }
+                //Si luego de recorrer la primera cadena la cantidad es distinto de cero, entonces no son anagramas.
+                if (cantidad != 0)
+                {
+                    Console.WriteLine("No son anagramas.");
+                }
+                //Si cantidad es cero, entonces son anagramas.
+                else
+                {
+                    Console.WriteLine("Las cadenas son anagramas.");
+                }
+            } 
+        }
+    }
+}
---
+++ Trabajo Practico 1/Entrega TP 1 - Iñaki Garro/Ejercicio1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Trabajo_Practico_1
+{
+    class Ejercicio1
+    {
+        static void Main()
+        {
+            //Ingreso por pantalla el numero que quiero controlar.
+            int numero = int.Parse(Console.ReadLine());
+            //Si el numero esta entre 1 y 9 inclusive, devolvera una cadena indicando que numero se ingreso.
+            if(numero == 1)
+            {
+                Console.WriteLine("Uno");
+            }
+            else if(numero == 2)
+            {
+                Console.WriteLine("Dos");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("Tres");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("Cuatro");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("Cinco");
+            }
+            else if (numero == 6)
+            {
+                Console.WriteLine("Seis");
+            }
+            else if (numero == 7)
+            {
+                Console.WriteLine("Siete");
+            }
+            else if (numero == 8)
+            {
+                Console.WriteLine("Ocho");
+            }
+            else if (numero == 9)
+            {
+                Console.WriteLine("Nueve");
+            }
+            //Si el numero es mayor a nueve, devuelve 'Otro'.
+            else if (numero > 9)
+            {
+                Console.WriteLine("Otro");
+            }
+        }
+    }
+}
